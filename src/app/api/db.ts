7a814e4607efb78cc783fceec2a3b987da89f10b
Kourosh <kouroshsharifineyestani@gmail.com>
2024-12-22 17:04:02 +0330

    feat: add /app/api/db.ts for events and remove /src/data/events.ts

---
+++ src/app/api/db.ts
@@ -0,0 +1,114 @@
+const events = [
+    {
+        id: 1,
+        name: "Event 1000",
+        date: "2024-12-01",
+        description: "Description of event 1",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+    {
+        id: 2,
+        name: "Event 2",
+        date: "2024-12-02",
+        description: "Description of event 2",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+    {
+        id: 3,
+        name: "Event 3",
+        date: "2024-12-03",
+        description: "Description of event 3",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+    {
+        id: 4,
+        name: "Event 4",
+        date: "2024-12-04",
+        description: "Description of event 4",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+    {
+        id: 5,
+        name: "Event 5",
+        date: "2024-12-05",
+        description: "Description of event 5",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+    {
+        id: 6,
+        name: "Event 6",
+        date: "2024-12-06",
+        description: "Description of event 6",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+    {
+        id: 7,
+        name: "Event 7",
+        date: "2024-12-07",
+        description: "Description of event 7",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+    {
+        id: 8,
+        name: "Event 8",
+        date: "2024-12-08",
+        description: "Description of event 8",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+    {
+        id: 9,
+        name: "Event 9",
+        date: "2024-12-09",
+        description: "Description of event 9",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+    {
+        id: 10,
+        name: "Event 10",
+        date: "2024-12-10",
+        description: "Description of event 10",
+        gallery: [
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+            "https://placehold.co/600x400.png",
+        ],
+    },
+];
+
+export default events;
