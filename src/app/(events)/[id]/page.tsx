d4551621e4b20abb7a588906f8365f9cd16c0121
Kourosh <kouroshsharifineyestani@gmail.com>
2024-12-22 17:26:14 +0330

    refactor: update event details page to fetch data from API dynamically

--- src/app/(events)/[id]/page.tsx
+++ src/app/(events)/[id]/page.tsx
@@ -1,18 +1,43 @@
-import { events } from "@/data/events";
+"use client";
+
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
 interface PageProps {
     params: Promise<{ id: string }>;
 }
 
-const Page = async ({ params }: PageProps) => {
-    const { id } = await params;
+interface Event {
+    name: string;
+    date: string;
+    description: string;
+    gallery: string[];
+}
 
-    // Find the correct event based on ID from URL params
-    const event = events.find((event) => event.id === parseInt(id));
+const Page = ({ params }: PageProps) => {
+    const [event, setEvent] = useState<Event | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
-    if (!event) {
-        return <div>Not found :/</div>;
-    }
+    useEffect(() => {
+        const fetchEvent = async () => {
+            const { id } = await params;
+            setLoading(true);
+            try {
+                const response = await fetch(`/api/events/${id}`);
+                const data = await response.json();
+                setEvent(data);
+            } catch (error) {
+                console.error("Error fetching event:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchEvent();
+    }, [params]);
+
+    if (loading) return <div>Loading...</div>;
+    if (!event) return <div>Event not found</div>;
 
     return (
         <div>
@@ -20,15 +45,15 @@ const Page = async ({ params }: PageProps) => {
             <p>{new Date(event.date).toLocaleDateString()}</p>
             <p>{event.description}</p>
             {event.gallery && event.gallery.length > 0 && (
-                <div>
+                <div className="flex flex-col gap-6">
                     {event.gallery.map((src, index) => (
-                        <img
+                        <Image
                             key={index}
                             src={src}
                             alt="alternative"
                             width={600}
                             height={400}
-                            className="rounded-2xl object-cover"
+                            className="max-w-full rounded-2xl object-cover"
                         />
                     ))}
                 </div>
